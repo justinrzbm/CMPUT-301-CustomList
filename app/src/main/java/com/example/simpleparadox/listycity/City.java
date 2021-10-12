68efeb30b20765c4da2260d13ceac0afc7780f72
justinrzbm <wrozeboo@ualberta.ca>
2021-10-12 17:36:30 -0600

    before deleting mainactivity and customlist

--- app/src/main/java/com/example/simpleparadox/listycity/City.java
+++ app/src/main/java/com/example/simpleparadox/listycity/City.java
@@ -1,12 +1,17 @@
 package com.example.simpleparadox.listycity;
 
-public class City {
+public class City implements Comparable<City>{
     private String city;
     private String province;
 
     City(String city, String province){
         this.city = city;
         this.province = province;
+    }
+
+    @Override
+    public int compareTo(City city) {
+        return this.city.compareTo(city.getCityName());
     }
 
     String getCityName(){
